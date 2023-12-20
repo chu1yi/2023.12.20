a93f0ca2c37ceec641a50ff035191af86c5c9a80
LE402N2027\uer <a0983779929@gmail.com>
2023-12-20 16:20:51 +0800

    App增加一張圖

--- server.R
+++ server.R
@@ -17,3 +17,37 @@ shinyServer(function(input, output) {
   })
   
 })
+
+library(shiny)
+
+shinyServer(function(input, output) {
+  
+  DATA = reactive({
+    if (is.null(input$files)) {return()} else {
+      dat = read.table(input$files$datapath,header=T)
+      return(dat) 
+    }
+  })
+  
+  output$summary = renderPrint({
+    dat = DATA()
+    if (is.null(dat)) {return("You have to up load your data!!!")} else {
+      summary(dat)
+    }
+  })
+  
+  output$scatterPlot = renderPlot({
+    dat = DATA()
+    if (is.null(dat)) {return()} else {
+      plot(dat,col=input$Color)
+    }
+  })
+  
+  output$view = renderTable({
+    dat = DATA()
+    if (is.null(dat)) {return()} else {
+      head(dat,input$n) 
+    }
+  })
+  
+})

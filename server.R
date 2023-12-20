649ed890d047a22ff295fe63b6a6ed433e5899bb
LE402N2027\uer <a0983779929@gmail.com>
2023-12-20 16:46:02 +0800

    使用互動物件

--- server.R
+++ server.R
@@ -51,3 +51,35 @@ shinyServer(function(input, output) {
   })
   
 })
+
+library(shiny)
+
+shinyServer(function(input, output) {
+  
+  DATA = reactive({
+    if (is.null(input$files)) {return()} else {
+      dat = read.table(input$files$datapath,header=T)
+      return(dat) 
+    }
+  })
+  
+  output$scatterPlot = renderPlot({
+    dat = DATA()
+    if (is.null(dat)) {return()} else {
+      plot(dat,col=input$Color)
+    }
+  })
+  
+  output$download = downloadHandler(
+    filename = function() {'plot.pdf'},
+    content = function(con) {
+      dat = DATA()
+      if (is.null(dat)) {return()} else {
+        pdf(con)
+        plot(dat,col=input$Color)
+        dev.off()
+      }
+    }
+  )
+  
+})
--- ui.R
+++ ui.R
@@ -37,3 +37,22 @@ shinyUI(pageWithSidebar(
   )  
   
 ))
+
+library(shiny)
+
+shinyUI(pageWithSidebar(
+  
+  headerPanel("Ploting your data"), 
+  
+  sidebarPanel(
+    fileInput(inputId="files", label=h4("Upload your data file:"), multiple=FALSE, accept="text/plain"),
+    helpText("Note: you only can upload the .txt file."),
+    radioButtons("Color", "Select the color of histogram:", choices = c("Red" = "red", "Blue" = "blue", "Green" = "green"))
+  ),
+  
+  mainPanel(
+    plotOutput("scatterPlot"),
+    downloadButton("download", label = "Download plot", class = NULL)
+  )  
+  
+))
